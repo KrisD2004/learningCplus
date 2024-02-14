0d538849954e7be8be9a0eddb126595224d85441
Kristian Dotson <KDotson387@gmail.com>
2024-02-14 16:54:02 -0600

    added chatcomments

--- WriteOutput.cpp
+++ WriteOutput.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
-// different ways to write output to console
+// This program demonstrates different ways to write output to the console
 
-using namespace std; // The 'using namespace std;' directive allows using elements from the 'std' namespace without explicitly specifying it
+// The 'using namespace std;' directive allows using elements from the 'std' namespace without explicitly specifying it
+using namespace std;
 
-int main()
-{
-    int x = 10; // Declare and initialize an integer variable 'x' with the value 10
+int main() {
+    // Declare and initialize an integer variable 'x' with the value 10
+    int x = 10;
 
+    // Declare and initialize another integer variable 'y' with the value 20
     int y = 20;
 
-    //// This is an example of chaining multiple insertion operations together in a single line.
+    // This is an example of chaining multiple insertion operations together in a single line.
     // It outputs the value of 'x' preceded by "x = " and the value of 'y' preceded by "y = " to the console.
     cout << "x = " << x << endl
-         << "y=" << y;
+         << "y = " << y;
 
-    return 0;     // Indicates successful program execution by returning 0 to the operating system
-
+    // Indicates successful program execution by returning 0 to the operating system
+    return 0;
 }
