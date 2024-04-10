66941e0ba0ec96b9ba3cad48ed8689e00051e238
Kristian Dotson <KDotson387@gmail.com>
2024-04-10 14:53:51 -0500

    arraycpp

---
+++ arraytut.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//Arrays
+int main () {
+
+    
+    int tipOptions[3] = {12, 13, 15};
+
+
+    cout << tipOptions[0] << endl;
+    cout << tipOptions[1] << endl;
+    cout << tipOptions[2]<< endl;
+
+    return 0;
+}
